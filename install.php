979865e00751d480f3c27fe47d5a573d320f54c3
Alexander Walther <aw@alexplus.de>
2024-02-07 15:16:09 +0100

    Update install.php

--- install.php
+++ install.php
@@ -4,7 +4,7 @@
 if(rex_addon::get('cronjob') && rex_addon::get('cronjob')->isAvailable()) {
     $cronjob = array_filter(rex_sql::factory()->getArray("SELECT * FROM rex_cronjob WHERE `type` = 'rex_cronjob_neues_publish'"));
     if (!$cronjob) {
-        $query = rex_file::get(__DIR__ . 'install/rex_cronjob_yform_auto_delete.sql');
+        $query = rex_file::get(__DIR__ . '/install/rex_cronjob_yform_auto_delete.sql');
         rex_sql::factory()->setQuery($query);
     }
 }
