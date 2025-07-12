47fd41d01a3c13d685f2624bfd6175d17305d520
Alexander Walther <aw@alexplus.de>
2025-07-12 21:17:48 +0200

    Auf Cronjobs hinweisen
    close #4

--- install.php
+++ install.php
@@ -1,10 +1,13 @@
 <?php
 
+$addon = rex_addon::get('cronjob');
+
 /* Cronjob installieren */
-if (rex_addon::get('cronjob') && rex_addon::get('cronjob')->isAvailable()) {
+if ($addon !== null && rex_addon::get('cronjob')->isAvailable()) {
     $cronjob = array_filter(rex_sql::factory()->getArray("SELECT * FROM rex_cronjob WHERE `type` = 'rex_cronjob_yform_auto_delete'"));
     if (!$cronjob) {
         $query = rex_file::get(__DIR__ . '/install/rex_cronjob_yform_auto_delete.sql');
         rex_sql::factory()->setQuery($query);
+        $addon->setProperty('successmsg', '<br><strong>' . rex_i18n::msg("auto_delete.auto_install") . '</strong>');
     }
 }
