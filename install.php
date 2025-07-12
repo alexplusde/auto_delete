77526fad3ca3ee66cb829fac6c2ce38435389dc0
alxndr-w <alxndr-w@users.noreply.github.com>
2025-07-12 19:18:32 +0000

    Apply php-cs-fixer changes

--- install.php
+++ install.php
@@ -3,11 +3,11 @@
 $addon = rex_addon::get('cronjob');
 
 /* Cronjob installieren */
-if ($addon !== null && rex_addon::get('cronjob')->isAvailable()) {
+if (null !== $addon && rex_addon::get('cronjob')->isAvailable()) {
     $cronjob = array_filter(rex_sql::factory()->getArray("SELECT * FROM rex_cronjob WHERE `type` = 'rex_cronjob_yform_auto_delete'"));
     if (!$cronjob) {
         $query = rex_file::get(__DIR__ . '/install/rex_cronjob_yform_auto_delete.sql');
         rex_sql::factory()->setQuery($query);
-        $addon->setProperty('successmsg', '<br><strong>' . rex_i18n::msg("auto_delete.auto_install") . '</strong>');
+        $addon->setProperty('successmsg', '<br><strong>' . rex_i18n::msg('auto_delete.auto_install') . '</strong>');
     }
 }
