5f2f2edcd4cf7b538e40e5c1f210583498a30993
Alexander Walther <aw@alexplus.de>
2025-07-13 14:32:47 +0200

    update install.php & sql (neuer Namespace)

--- install.php
+++ install.php
@@ -4,7 +4,7 @@ $addon = rex_addon::get('cronjob');
 
 /* Cronjob installieren */
 if (null !== $addon && rex_addon::get('cronjob')->isAvailable()) {
-    $cronjob = array_filter(rex_sql::factory()->getArray("SELECT * FROM rex_cronjob WHERE `type` = 'rex_cronjob_auto_delete_yform'"));
+    $cronjob = array_filter(rex_sql::factory()->getArray("SELECT * FROM rex_cronjob WHERE `type` = 'Alexplusde\AutoDelete\Cronjob\YFormTable'"));
     if (!$cronjob) {
         $query = rex_file::get(__DIR__ . '/install/rex_cronjob_yform_auto_delete.sql');
         rex_sql::factory()->setQuery($query);
