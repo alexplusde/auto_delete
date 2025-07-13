a82d247e453d378317522e82f6aa68c99367be2b
Alexander Walther <aw@alexplus.de>
2025-07-13 14:08:59 +0200

    fix boot.php

--- boot.php
+++ boot.php
@@ -7,9 +7,9 @@ use rex_addon;
 use rex_cronjob_manager;
 
 if (rex_addon::get('cronjob')->isAvailable() && !rex::isSafeMode()) {
-    rex_cronjob_manager::registerType(Table::class);
-    rex_cronjob_manager::registerType(YFormTable::class);
-    rex_cronjob_manager::registerType(Folder::class);
+    rex_cronjob_manager::registerType(Cronjob\Table::class);
+    rex_cronjob_manager::registerType(Cronjob\YFormTable::class);
+    rex_cronjob_manager::registerType(Cronjob\Folder::class);
 }
 
 // auto_delete::writeCronjob();
